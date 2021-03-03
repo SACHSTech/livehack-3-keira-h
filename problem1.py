33531819f86e935420211f1cd1273d16efc6e342
Keira Hosey <keira.hosey23@ycdsbk12.ca>
2021-03-03 15:22:08 +0000

    Final Submission

--- problem1.py
+++ problem1.py
@@ -1,1 +1,31 @@
+"""
+----------------------------------------------------------
+Name:    problem1.py
+Purpose: Write a program to determine which group a player is placed in
 
+Author:  Hosey.K
+
+Created: date in 03/03/2021
+----------------------------------------------------------
+"""
+
+# Header of tracker
+print("~~~~~~~~~~~~ Tournament Tracker ~~~~~~~~~~~~")
+
+# Input wins and losses
+win_count = 0
+
+for i in range(6):
+  game = input("Enter the wins and losses for your team: ")
+  if game == "W":
+    win_count += 1
+
+# Determining Group
+if win_count >= 5:
+  print("Your team is in Group 1.")
+elif (win_count == 3) or (win_count == 4):
+  print("Your team is in Group 2.")
+elif (win_count == 1) or (win_count == 2):
+  print("Your team is in Group 3.")
+else:
+  print("Your team is eliminated from the tournament.")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,39 @@
+"""
+----------------------------------------------------------
+Name:    problem2.py
+Purpose: Write a program that allows Tony to enter the distance driven for each day until the total distance driven surpasses 100km
 
+Author:  Hosey.K
+
+Created: date in 03/03/2021
+----------------------------------------------------------
+"""
+
+# Header of tracker
+print("~~~~~~~~~ Welcome to the DoorDash Distance Tracker ~~~~~~~~~")
+print("")
+
+# Travel log title
+print("~~~~~~ Travel Log ~~~~~~")
+
+# Input distance travelled
+number_days = 0
+distance_per_day = 0 
+total_distance = 0
+
+while distance_per_day < 100:
+  distance_per_day = float(input("Enter the distance travelled for the day: "))
+  number_days += 1
+  total_distance += distance_per_day
+  if total_distance > 100:
+    break
+
+# Summary title
+print("")
+print("~~~~~~~ Summary ~~~~~~~")
+
+# Output Message
+print("It took",number_days,"days to surpass 100 km driven.")
+
+average = total_distance//number_days
+print("The average distance driven per day is",average,"km.")
